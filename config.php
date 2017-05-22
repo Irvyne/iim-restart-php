b189b95fbb58c3fd9b1f358c6206d2f453a786a1
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-05-22 12:59:32 +0200

    update

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$pdo = new PDO('mysql:host=localhost;dbname=php_rick_and_morty', 'root', 'toor');
---
+++ dump.php
@@ -0,0 +1,42 @@
+<?php
+
+require "vendor/autoload.php";
+
+$cities = [
+    "London" => [
+        'inhabitants' => 500000,
+        'Country' => "UK",
+    ],
+    "Lotrzadzndon" => [
+        'inhabitants' => 500000,
+        'Country' => "UK",
+    ],
+    "Lorthrtndon" => [
+        'inhabitants' => 500000,
+        'Country' => "UK",
+    ],
+    "Lotrhgtrhndon" => [
+        'inhabitants' => 500000,
+        'Country' => "UK",
+    ],
+    "Lonregergdon" => [
+        'inhabitants' => 500000,
+        'Country' => "UK",
+    ],
+    "Londorefzefn" => [
+        'inhabitants' => 500000,
+        'Country' => "UK",
+    ],
+    "Londonefzef" => [
+        'inhabitants' => 500000,
+        'Country' => "UK",
+    ],
+];
+
+//print_r($cities);
+//var_dump($cities);
+
+
+
+dump($cities);
+
